7269b19e3e23ea5e48f094083bd9a8f9c969ecb1
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-05-09 16:04:00 -0400

    Added Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import React from "react";
+
+export default function Home() {
+	return (
+		<React.Fragment>
+			{" "}
+			<h1>
+				
+			</h1>
+		</React.Fragment>
+	);
+}

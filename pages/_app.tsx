8d738e2adffe4d49732c12390d69dceb670d266b
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-05-09 20:33:34 -0400

    Setup Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+	colors: {
+		brand: {
+			100: "#003049",
+			200: "#D62828",
+			300: "#F77F00",
+			400: "#FCBF49",
+			500: "#EAE2B7"
+		}
+	}
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
